82410eba35d6f910be376209b7915e2db25ccef3
Joshua <kenduajayi@gmail.com>
2023-08-09 05:57:37 -0700

    Parv: modified authors file

---
+++ models/engine/file_store.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This Imports some Standard modules and modules from the project packages"""
+import json
+from models.base_model import BaseModel as bm
+
+"""
+This is the Python class that will be responsible for the file storage.
+"""
+
+
+class FileStorage():
+    """
+    This is the class responsible for data storage for AirBnB Clone project.
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        ...
+
+    def new(self):
+        ...
+
+    def save(self):
+        ...
+
+    def reload(self):
+        ...
