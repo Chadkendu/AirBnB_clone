f7961a46f95da60e426ddb3ef20b5bc9516b9d72
Noha Fathy <nohazaid97@gmail.com>
2023-08-11 04:44:15 -0700

    Noha: made some files executable

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,14 +15,29 @@ class FileStorage():
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        ...
+    def all(self) -> dict:
+        return self.__objects
 
-    def new(self):
-        ...
+    def new(self, obj: dict) -> None:
+    """
+    This is the public instance of the method that sets in `__objects` `obj` with
+    the key
+    Args:
+     obj (dict) - the dictionaary object of the file
+    """
+    fmt = "{}.{}".format(obj.__class__.__name__, obj.id)
+    self.__objects[fmt] = obj
 
-    def save(self):
-        ...
-
-    def reload(self):
+    def reload(self) -> None:
+    """
+    This is the public instance of the method that deserializes the json string into
+    a dictionary object `__objects` only if the  `__file_path` exist.
+    """
+    try:
+        with open(self.__file_path, mode="r", encoding="utf-8") as fn:
+            data_strm = json.load(fn)
+        for key, val in data_strm.items():
+            class_name = key.split(".")[0]
+            self.new(eval(class_name + "(**val)"))
+    except:
         ...
