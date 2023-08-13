9b0ea470c886f0a6be48d7bd1f2ace579827f44f
Noha Fathy <nohazaid97@gmail.com>
2023-08-13 05:58:49 -0700

    Noha: fixed pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,22 +26,22 @@ class FileStorage():
         return FileStorage.__objects
 
     def new(self, obj: dict) -> None:
-    """
-    The public instance of the method that sets in `__objects` `obj` with
-    the key
-    Args:
-    obj (dict) - the dictionaary object of the file
-    """
+        """
+        The public instance of the method that sets in `__objects` `obj` with
+        the key
+        Args:
+        obj (dict) - the dictionaary object of the file
+        """
 
     obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
     FileStorage.__objects[obj_key] = obj
 
     def save(self) -> None:
-    """
-    This is a public instance method that serializes the private instance
-    object `__objects` (dict) into a JSON string and save it to a flat
-    database (json file)
-    """
+        """
+        This is a public instance method that serializes the private instance
+        object `__objects` (dict) into a JSON string and save it to a flat
+        database (json file)
+        """
     dict_serial = {}
     with open(self.__file_path, mode="w", encoding="utf-8") as file_obj:
         for key, val in self.__objects.items():
@@ -49,10 +49,10 @@ class FileStorage():
             json.dump(dict_serial, file_obj)
 
     def reload(self) -> None:
-    """
-    The public instance of method that deserializes the json string into
-    a dictionary object `__objects` only if the  `__file_path` exist.
-    """
+        """
+        The public instance of method that deserializes the json string into
+        a dictionary object `__objects` only if the  `__file_path` exist.
+        """
     try:
         path = self.__file_path
         with open(path, mode="r", encoding="utf-8") as file_obj:
